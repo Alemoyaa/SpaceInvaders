b99d4015969f3af70bb2ee165f6d5f30fb3c9684
Alejandro Moya <Alejandro5320@gmail.com>
2019-03-29 20:35:46 -0300

    Version 0.2 Con el gif ya cargado

--- src/Main/Invaders.java
+++ src/Main/Invaders.java
@@ -10,60 +10,64 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.net.URL;
+import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Invaders extends Canvas{
-    public static final int WIDTH = 800;//Largo
-    public static final int HEIGHT = 600;//Alto
-    
-    public BufferedImage bicho = null;
-    
-    public Invaders(){
-        JFrame ventana = new JFrame("Invaders");
-        
-        JPanel panel = (JPanel)ventana.getContentPane();
-        setBounds(0, 0, WIDTH, HEIGHT);//Definimos tamaño de la ventana
-        panel.setPreferredSize(new Dimension(WIDTH,HEIGHT));
-        panel.setLayout(null);//Al desactivar el layout debemos ingresar las coordenadas de las cosas que deseamos ponerle al panel
-        panel.add(this);
-        ventana.setBounds(0, 0, WIDTH, HEIGHT);
-        ventana.setVisible(true);
-        ventana.setLocationRelativeTo(null);
-        
-        ventana.addWindowListener(new WindowAdapter(){ //Con esto agregamos un evento para que cuando cerremos la ventana el programa se deje de ejecutar
-            @Override
-            public void windowClosing(WindowEvent e){//Este es el evento 
-            System.exit(0);//Cierre el programa
-            }
-        });
-    }
-    
-    public BufferedImage loadImage(String nombre){
-        
-        URL url=null; //Ni idea que hace 
-        try{
-            url = getClass().getClassLoader().getResource(nombre);//permite obtener una URL apuntando a un subdirectorio relativo al sitio del cual fue cargada la clase. Esto permite que nuestro programa funcione de forma transparente
-            return ImageIO.read(url);
-        } catch(Exception e){
-            System.out.println("No se puedo carcar la imagen "+nombre+" de "+url);
-            System.out.println("El error fue : "+e.getClass().getName()+" "+e.getMessage());
-            System.exit(0);
-            return null;
-        }
-    }
-    
-    @Override
-    public void paint(Graphics g){
-        if(bicho == null) 
-            bicho = loadImage("/src/res/bicho.gif");
-        
-        g.drawImage(bicho, 40, 40, this);
-    }
-    
-    public static void main(String[] args){
-        Invaders inv = new Invaders();
-    }
+public class Invaders extends Canvas {
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 600;
+	
+	public HashMap sprites;
+	
+	public Invaders() {
+		sprites = new HashMap();
+		
+		JFrame ventana = new JFrame("Invaders");
+		JPanel panel = (JPanel)ventana.getContentPane();
+		setBounds(0,0,WIDTH,HEIGHT);
+		panel.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		panel.setLayout(null);
+		panel.add(this);
+		ventana.setBounds(0,0,WIDTH,HEIGHT);
+		ventana.setVisible(true);
+		ventana.addWindowListener( new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+	
+	public BufferedImage loadImage(String nombre) {
+		URL url=null;
+		try {
+			url = getClass().getClassLoader().getResource(nombre);
+			return ImageIO.read(url);
+		} catch (Exception e) {
+			System.out.println("No se pudo cargar la imagen " + nombre +" de "+url);
+			System.out.println("El error fue : "+e.getClass().getName()+" "+e.getMessage());
+			System.exit(0);
+			return null;
+		}
+	}
+	
+	public BufferedImage getSprite(String nombre) {
+		BufferedImage img = (BufferedImage)sprites.get(nombre);
+		if (img == null) {
+			img = loadImage("res/"+nombre);
+			sprites.put(nombre,img);
+		}
+		return img;
+	}
+	
+	
+	public void paint(Graphics g) {
+		g.drawImage(getSprite("bicho.gif"), 40, 40,this);
+	}
+	
+	public static void main(String[] args) {
+		Invaders inv = new Invaders();
+	}
 }
